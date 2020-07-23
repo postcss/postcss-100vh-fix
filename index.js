80caf7d972c24e3c822b5b5366cead590450f87b
Andrey Sitnik <andrey@sitnik.ru>
2020-07-22 23:10:31 -0700

    Fix source maps

--- index.js
+++ index.js
@@ -8,21 +8,21 @@ module.exports = postcss.plugin('postcss-100vh-fix', () => {
 
       let media = postcss.atRule({
         name: 'supports',
-        params: '(-webkit-touch-callout: none)'
-        // source: decl.source
+        params: '(-webkit-touch-callout: none)',
+        source: decl.source
       })
       rule.after(media)
 
       let clonedRule = postcss.rule({
-        selector: rule.selector
-        // source: rule.source
+        selector: rule.selector,
+        source: rule.source
       })
       media.append(clonedRule)
 
       clonedRule.append({
         prop: decl.prop,
-        value: '-webkit-fill-available'
-        // source: decl.source
+        value: '-webkit-fill-available',
+        source: decl.source
       })
     })
   }
