94c35c9d61cf327137a55f129237f57fa5ffacb9
Andrey Sitnik <andrey@sitnik.ru>
2020-08-20 21:52:43 -0500

    Update plugin for PostCSS 8

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
-let postcss = require('postcss')
+function process (decl, { AtRule, Rule }) {
+  if (decl.value !== '100vh') return
+  let rule = decl.parent
 
-module.exports = postcss.plugin('postcss-100vh-fix', () => {
-  return root => {
-    root.walkDecls(/(min-|max-)?height/, decl => {
-      if (decl.value !== '100vh') return
-      let rule = decl.parent
+  let media = new AtRule({
+    name: 'supports',
+    params: '(-webkit-touch-callout: none)',
+    source: decl.source
+  })
+  rule.after(media)
 
-      let media = postcss.atRule({
-        name: 'supports',
-        params: '(-webkit-touch-callout: none)',
-        source: decl.source
-      })
-      rule.after(media)
+  let clonedRule = new Rule({
+    selector: rule.selector,
+    source: rule.source
+  })
+  media.append(clonedRule)
 
-      let clonedRule = postcss.rule({
-        selector: rule.selector,
-        source: rule.source
-      })
-      media.append(clonedRule)
+  clonedRule.append({
+    prop: decl.prop,
+    value: '-webkit-fill-available',
+    source: decl.source
+  })
+}
 
-      clonedRule.append({
-        prop: decl.prop,
-        value: '-webkit-fill-available',
-        source: decl.source
-      })
-    })
+module.exports = () => {
+  return {
+    postcssPlugin: 'postcss-100vh-fix',
+    Declaration: {
+      'min-height': process,
+      'max-height': process,
+      'height': process
+    }
   }
-})
+}
+module.exports.postcss = true
--- index.test.js
+++ index.test.js
@@ -3,7 +3,7 @@ let postcss = require('postcss')
 let plugin = require('./')
 
 function run (input, output) {
-  let result = postcss([plugin()]).process(input, { from: undefined })
+  let result = postcss([plugin]).process(input, { from: undefined })
   expect(result.css).toEqual(output)
   expect(result.warnings()).toHaveLength(0)
 }
